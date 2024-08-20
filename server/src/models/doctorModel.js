816a1a61c5a1cadb9ea23baf5b672c9f3d7b4ce1
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-08-20 23:29:07 +0600

    department and doctor model make

---
+++ server/src/models/doctorModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const doctorSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    department:{
+        type:String,
+        required:true,
+    },
+
+    phone:{
+        type:String,
+        required:true,
+    },
+    designation:{
+        type:String,
+        required:true
+    },
+    image:{
+        type:String,
+
+    }
+});
+
+const Doctor = mongoose.model('Doctor',doctorSchema);
+
+export default Doctor;
